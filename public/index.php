35f5498a180337e9a6374e7f3402f54be6c4970d
rabeeiiii <mohamedrabeei2903@gmail.com>
2023-10-20 22:21:05 +0300

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,14 @@
+// Load Config
+require_once '../config/config.php';
+require_once '../vendor/autoload.php';
+<?php
+
+// Autoloader
+require_once '../vendor/autoload.php';
+
+// Load Config
+require_once '../config/config.php';
+
+// Routes
+require_once '../routes/web.php';
+require_once '../app/Router.php';
