034cdacaaf73131c23cff5a41738b0590f8ccdf7
rabeeiiii <mohamedrabeei2903@gmail.com>
2023-10-20 22:21:18 +0300

    routes

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php 
+
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+// Routes system
+$routes = new RouteCollection();
+$routes->add('product', new Route(constant('URL_SUBFOLDER') . '/product/{id}', 
+array('controller' => 'ProductController', 'method'=>'showAction'), array('id' => '[0-9]+')));
+$routes->add('homepage', new Route(constant('URL_SUBFOLDER') . '/',
+ array('controller' => 'PageController', 'method'=>'indexAction'), array()));
